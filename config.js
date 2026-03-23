aef7f6c915f9e465185564f6a19881819af770b1
Reigar2011 <nu2430605@gmail.com>
2026-03-23 09:32:32 -0400

    Update global.owner array with new entries

--- config.js
+++ config.js
@@ -13,7 +13,7 @@ import { ar, en, es, id, pt } from './lib/idiomas/total-idiomas.js'
 // [ES] > Agrega el número que será Bot y los que serán propietarios.
 // [EN] > Add the number that will be Bot and those that will be owners.
 global.owner = [
-['584241184114', 'Mi nombre', true]
+['584241184114', 'Mi nombre', true],
 ['593968263524'],
 ['5492916450307'],
 ['5215539356057']
