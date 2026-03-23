262f2df682b55659122799b465e39c1b6be21f65
Reigar2011 <nu2430605@gmail.com>
2026-03-23 09:30:40 -0400

    Update config.js

--- config.js
+++ config.js
@@ -13,10 +13,10 @@ import { ar, en, es, id, pt } from './lib/idiomas/total-idiomas.js'
 // [ES] > Agrega el número que será Bot y los que serán propietarios.
 // [EN] > Add the number that will be Bot and those that will be owners.
 global.owner = [
-'5214531287294', 
-'593968263524',
-'5492916450307',
-'5215539356057'
+['584241184114', 'Mi nombre', true]
+['593968263524'],
+['5492916450307'],
+['5215539356057']
 ]
 
 global.mods = []
